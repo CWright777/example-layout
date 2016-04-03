79c2eb52df12681a19e486c7af1ca10a35510b49
fordwright21 <fordwright@gmail.com>
2016-04-02 17:10:30 -0700

    add user methods

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -3,6 +3,16 @@ var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
 module.exports = {
+  index: function(req,res){
+    User.find({},function(err,users){
+      res.json(users);
+    })
+  },
+  show: function(req,res){
+    User.findOne({_id: req.params.id}, function(err,user){
+      res.json(user);
+    })
+  },
   create: function(req,res){
     var newUser = new User({
       name: req.body.name
